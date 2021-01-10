6ce7956d1b196d3fe86835a6ebbbe540709be987
nzoghb <nzoghb@users.noreply.github.com>
2021-01-10 12:14:30 -0800

    delete frontend

--- frontend/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { render } from "react-dom";
-
-import "bootstrap/dist/css/bootstrap.css";
-
-import App from "./App";
-
-render(<App />, document.getElementById("app"));
